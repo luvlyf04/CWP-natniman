1f80bb5734460d39af5cb46113159a0eae05c176
luvlyf04 <maysa250404@gmail.com>
2026-09-21 21:42:07 +0700

    cell00 - cell02

---
+++ discovery_piscine/cell01/ex02/age.py
@@ -0,0 +1,2 @@
+my_age = 67
+print(my_age)
---
+++ discovery_piscine/cell02/ex02/password.py
@@ -0,0 +1,7 @@
+password = "Python is awesome"
+
+input_data = str(input("Enter the password: "))
+if input_data == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
