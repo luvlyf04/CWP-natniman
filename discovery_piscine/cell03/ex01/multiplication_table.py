e1aeb421bd6d63557f73da664da2fd79f46cdfe5
luvlyf04 <maysa250404@gmail.com>
2026-09-23 21:50:04 +0700

    change range

--- discovery_piscine/cell03/ex01/multiplication_table.py
+++ discovery_piscine/cell03/ex01/multiplication_table.py
@@ -1,7 +1,7 @@
 try :
     input_number = int(input("Enter a number "))
 
-    for number in range(0, 9):
+    for number in range(0, 10):
         result = number * input_number
         print(f"{number} x {input_number} = {result}")
 
